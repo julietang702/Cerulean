4aca4035652995bcb954c1ca4622acda16c75d50
razsa <21493e@gmail.com>
2023-05-12 00:19:21 -0400

    kamil to the rescue

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
-// Get the url endpoint
-const sample = "https://raw.githubusercontent.com/nburwick/Cerulean/main/static/Resources/Tornado_Tracks.geojson";
+const sample =
+  "https://raw.githubusercontent.com/nburwick/Cerulean/main/static/Resources/Tornado_Tracks.geojson"
 
-// Get the .json data from the URL
-d3.json(sample)
-  .then(function(data) {
-    console.log(data);
-
-    createDropDown(data);
-  })
-  .catch(function(error) {
-    console.error(error);
-  });
 // Function to create the dropdown options
 function createDropDown(data) {
-  var features = data.features;
+  var features = data.features
 
   // Access the "yr" values and remove duplicates
-  var yrValues = [...new Set(features.map(feature => feature.properties.yr))];
+  var yrValues = [...new Set(features.map(feature => feature.properties.yr))]
 
   // Sort the "yr" values in ascending order
-  yrValues.sort(function(a, b) {
-    return d3.ascending(a, b);
-  });
+  yrValues.sort(function (a, b) {
+    return d3.ascending(a, b)
+  })
 
   // Create the dropdown options
   d3.select("#years")
@@ -29,21 +19,17 @@ function createDropDown(data) {
     .data(yrValues)
     .enter()
     .append("option")
-    .text(function(d) {
-      return d;
+    .text(function (d) {
+      return d
     })
-    .on("change", function() {
-      var year = d3.select(this).property("value");
-     
-    });
-} // <--- Add this closing curly bracket
-
+}
+console.log(createDropDown)
 // Fetch the data from the URL using d3.json
-d3.json("Tornado_Tracks.geojson").then(data => {
+d3.json(sample).then(data => {
   // Create a list of dictionaries
-  var tornadoes = [];
+  var tornadoes = []
   for (const feature of data.features) {
-    const properties = feature.properties;
+    const properties = feature.properties
     const tornado = {
       id: properties.OBJECTID,
       elat: properties.elat,
@@ -51,53 +37,132 @@ d3.json("Tornado_Tracks.geojson").then(data => {
       slat: properties.slat,
       slon: properties.slon,
       magnitude: properties.mag,
-      year: properties.yr
-    };
-    tornadoes.push(tornado);
+      year: properties.yr,
+    }
+    tornadoes.push(tornado)
   }
 
-  console.log(tornadoes);
+  // Get the selected year from the dropdown
+  var year = d3.select("#years").property("value")
 
-  // Create a Leaflet map and add the heat map layer
-  var myMap = L.map("map").setView([37.0902, -95.7129], 4);
+  // Filter the tornadoes array by the year
+  var filteredTornadoes = tornadoes.filter(function (tornado) {
+    return parseInt(tornado.year) == parseInt(year)
+  })
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
+  // Create an array of coordinate pairs for the filtered tornadoes
+  var coordinates = filteredTornadoes.map(function (tornado) {
+    return [tornado.slat, tornado.slon]
+  })
+
+  // Add the heatmap layer to the map
+  var heat = L.heatLayer(coordinates, { radius: 20 }).addTo(map)
+
+  // Update the heatmap layer when the year is changed
+  function updateHeatMap(year) {
+    // Filter the tornadoes array by the year
+    var filteredTornadoes = tornadoes.filter(function (tornado) {
+      return parseInt(tornado.year) == parseInt(year)
+    })
+
+    // Create an array of coordinate pairs for the filtered tornadoes
+    var filteredCoordinates = filteredTornadoes.map(function (tornado) {
+      return [tornado.slat, tornado.slon]
+    })
+
+    // Update the heat layer with the new coordinates
+    heat.setLatLngs(filteredCoordinates)
+  }
+
+  // Add event listener for the update button
+  document.getElementById("update-btn").addEventListener("click", function () {
+    var year = document.getElementById("years").value
+    updateHeatMap(year)
+  })
+})
+
+// Fetch the data from the URL using d3.json
+d3.json("Tornado_Tracks.geojson").then(data => {
+  // Create a list of dictionaries
+  var tornadoes = []
+  for (const feature of data.features) {
+    const properties = feature.properties
+    const tornado = {
+      id: properties.OBJECTID,
+      elat: properties.elat,
+      elon: properties.elon,
+      slat: properties.slat,
+      slon: properties.slon,
+      magnitude: properties.mag,
+      year: properties.yr,
+    }
+    tornadoes.push(tornado)
+  }
+
+  console.log(tornadoes)
+
+  // Create a Leaflet map object and add the heat map layer
+  var myMap = L.map("map").setView([37.0902, -95.7129], 4)
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(myMap)
 
   // Update the coordinates variable with latitudes and longitudes from the tornado data
-  coordinates = tornadoes.map(function (tornado) {
-    return [tornado.slat, tornado.slon, tornado.year];
-  });
+  var coordinates = tornadoes.map(function (tornado) {
+    return [tornado.slat, tornado.slon]
+  })
 
-  console.log(coordinates);
+  console.log(coordinates)
 
   // Create a heat map layer with a default filter for all years
-  let heat = L.heatLayer(coordinates.filter(function(tornado) {return tornado[2] >= 1950 && tornado[2] <= 2021}), {
-    radius: 25,
-    minOpacity : 0.4,
-    gradient:{0.4:'blue', 0.5:'lime', 0.6:'red'}}).addTo(myMap);
+  let heat = L.heatLayer(
+    coordinates.filter(function (tornado) {
+      return tornado[2] >= 1950 && tornado[2] <= 2021
+    }),
+    {
+      radius: 25,
+      minOpacity: 0.3,
+      gradient: {
+        0.1: "blue",
+        0.3: "green",
+        0.6: "yellow",
+        0.8: "orange",
+        1: "red",
+      },
+    }
+  ).addTo(myMap)
 
-  // Create a year range using d3
-  const yearExtent = d3.extent(tornadoes, d => d.year);
-  const yearRange = d3.range(yearExtent[0], yearExtent[1] + 1);
+  createDropDown(data)
+})
 
-  // Add a range input slider to filter the data by year
-  var slider = document.getElementById("year-range");
-  slider.min = yearExtent[0];
-  slider.max = yearExtent[1];
-  slider.value = yearExtent[0];
-  slider.oninput = function() {
-    // Update the heat map layer with the new filter
-    heat.setLatLngs(coordinates.filter(function(tornado) {return tornado[2] >= slider.value}));
+function updateHeatMap(year) {
+  // Get the selected year from the dropdown if not passed as an argument
+  if (!year) {
+    year = d3.select("#years").property("value")
   }
 
-});
+  // Filter the tornadoes array by the year
+  var filteredTornadoes = tornadoes.filter(function (tornado) {
+    return parseInt(tornado.year) == parseInt(year)
+  })
 
+  // Update the coordinates variable with the filtered values
+  var filteredCoordinates = filteredTornadoes.map(function (tornado) {
+    return [tornado.slat, tornado.slon]
+  })
 
+  // filter the data based on the selected year
+  var filteredData = data.filter(function (d) {
+    return d.year === year
+  })
 
+  // clear the previous layer and add new heatmap layer
+  if (heatmapLayer) {
+    map.removeLayer(heatmapLayer)
+  }
 
-
-  
-  
-
+  // Update the heat layer with the new coordinates
+  heat = L.heatLayer(filteredCoordinates, { radius: 20 }).addTo(map)
+}
